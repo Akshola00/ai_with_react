27cbae7a97297ec3fe866bc3ddfa6f90f34355d6
Akshola00 <akinniyishola07@gmail.com>
2024-02-26 20:13:45 +0100

    progress

--- AI/src/pages/Home.jsx
+++ AI/src/pages/Home.jsx
@@ -6,7 +6,7 @@ import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
 function Home() {
   return (
     <div>
-      <div className="grid grid-cols-2 grid-rows-5 gap-4">
+      <div className="grid grid-cols-2 maingrid gap-4">
         {/* line 1 */}
         <div className="m-9 p-4 w-fit">
           <div className="flex flex-col ">
@@ -67,8 +67,8 @@ function Home() {
           </div>
         </div>
         <div className=" rounded-2xl flex items-center justify-center formbg">
-          <div className="myoverlay"></div> 
-           <form
+          <div className="myoverlay"></div>
+          <form
             className="p-8 font-Fredoka flex flex-col gap-6 w-1/2 bg-[color:var(--body-backgorund)] text-[color:var(--text-color)] rounded-xl sd"
             action=""
           >
@@ -109,30 +109,62 @@ function Home() {
             >
               FREE TRIAL
             </button>
-          </form> 
+          </form>
         </div>
 
         {/* line 2 */}
-        <div className="master items-center justify-center flex gap-5 font-Fredoka bg-blue-400">
-          <div> 3800+ USER ACTIVE </div>
+        <div className="master items-center text-3xl justify-center flex gap-12 font-Fredoka text-[color:var(--text-color)] pt-16 pb-16">
+          <div>
+            {" "}
+            3800+{" "}
+            <span className=" inline-block text-transparent bg-clip-text tbg">
+              USER ACTIVE{" "}
+            </span>{" "}
+          </div>
           <span> | </span>
-          <div> 230+ TRUSTED BY COMPANY </div>
+          <div>
+            {" "}
+            230+{" "}
+            <span className=" inline-block text-transparent bg-clip-text tbg">
+              TRUSTED BY COMPANY{" "}
+            </span>{" "}
+          </div>
           <span> | </span>
-          <div> $230M+ TRANSACTION </div>
-          
+          <div>
+            {" "}
+            $230M+{" "}
+            <span className=" inline-block text-transparent bg-clip-text tbg">
+              {" "}
+              TRANSACTION{" "}
+            </span>{" "}
+          </div>
         </div>
-        <div>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi
-          accusantium maxime autem illum. Atque, repellat consequatur pariatur
-          soluta error amet, non aperiam esse aliquam quos ipsum mollitia sint,
-          architecto maxime.
+        <div className="p-4  m-9 text-[color:var(--text-color)]">
+          <div className=" flex items-start flex-col gap-3 w-3/4">
+            <p className=" font-Varela text-4xl leading-snug tracking-wide">
+              You do the business, <br /> we'll handle the money.
+            </p>
+            <p className=" font-Fredoka text-lg">
+              with the right credit card, you can improve your fainancial life
+              by building credit, earning rewards and saving money. But with
+              hundreds of credit cards on the market
+            </p>
+            <button className=" pt-2 pb-2 pl-6 pr-6 text-lg font-Fredoka rounded-lg bg-[color:var(--link-color)] text-white">
+              Get started
+            </button>
+          </div>
         </div>
 
         <div>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi
-          accusantium maxime autem illum. Atque, repellat consequatur pariatur
-          soluta error amet, non aperiam esse aliquam quos ipsum mollitia sint,
-          architecto maxime.
+          <div>
+            <div>
+              <img src="" alt="" />
+            </div>
+            <div>
+              <p>Rewards</p>
+              <p>The best credit cards offer some tantalizing combinatuions o fpromotions and proces</p>
+            </div>
+          </div>
         </div>
         <div>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi
