8d636f41c8556eb07ca84c94451078e4fbcac506
Akshola00 <akinniyishola07@gmail.com>
2024-02-27 20:09:24 +0100

    progress

--- AI/src/pages/Home.jsx
+++ AI/src/pages/Home.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import "./Home.css";
 import logo from "./logo (2).png";
+import star from "./star.png";
+import send from "./send.png";
+import folder from "./folder.png";
+import p1 from "./luis-villasmil-hh3ViD0r0Rc-unsplash.jpg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
+import {
+  faArrowUpRightFromSquare,
+  faStar,
+} from "@fortawesome/free-solid-svg-icons";
 function Home() {
   return (
     <div>
@@ -155,28 +162,121 @@ function Home() {
           </div>
         </div>
 
-        <div>
-          <div>
-            <div>
-              <img src="" alt="" />
+        <div className=" font-Fredoka flex flex-col gap-4 justify-center items-center">
+          <div className="  flex gap-4  p-3 mybxw bg-slate-100 rounded-3xl ">
+            <div className=" tbg rounded-full justify-center items-center w-16 h-16 flex">
+              <img className=" w-10" src={star} alt="" />
             </div>
-            <div>
-              <p>Rewards</p>
-              <p>The best credit cards offer some tantalizing combinatuions o fpromotions and proces</p>
+            <div className="flex flex-col w-96 ">
+              <p className="text-xl">Rewards</p>
+              <p className="">
+                The best credit cards offer some tantalizing combinatuions of
+                promotions and process
+              </p>
+            </div>
+          </div>
+          <div className="  flex gap-4  p-3 mybxw linksbg text-white rounded-3xl ">
+            <div className=" tbg rounded-full justify-center items-center w-16 h-16 flex">
+              <img className=" w-10" src={send} alt="" />
+            </div>
+            <div className="flex flex-col w-96 ">
+              <p className="text-xl">Balance Transfer</p>
+              <p className="">
+                A balance transfer credit card can save you alot of money in
+                intrest charges
+              </p>
+            </div>
+          </div>
+          <div className="flex gap-4  p-3 mybxw bg-slate-100 rounded-3xl ">
+            <div className=" tbg rounded-full justify-center items-center w-16 h-16 flex">
+              <img className=" w-10" src={folder} alt="" />
+            </div>
+            <div className="flex flex-col w-96 ">
+              <p className="text-xl">100% Secured</p>
+              <p className="">
+                We take proactive steps make sure your information and
+                transactions are secure
+              </p>
             </div>
           </div>
         </div>
-        <div>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi
-          accusantium maxime autem illum. Atque, repellat consequatur pariatur
-          soluta error amet, non aperiam esse aliquam quos ipsum mollitia sint,
-          architecto maxime.
+
+        <div className="master p-14  text-[color:var(--text-color)] flex justify-between items-center">
+          <div className=" font-Varela text-6xl">
+            What people are <br /> saying about us
+          </div>
+          <div className=" font-Fredoka text-2xl mr-20">
+            Anything you need to accept card payments <br /> and grow your
+            buisness anywhere on the planet.
+          </div>
         </div>
-        <div>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi
-          accusantium maxime autem illum. Atque, repellat consequatur pariatur
-          soluta error amet, non aperiam esse aliquam quos ipsum mollitia sint,
-          architecto maxime.
+        <div className="master pt-10 pl-10 pr-0 pb-0 text-center bg-slate-400 text-amber-500">
+          <div className="inner max-w-7xl m-auto overflow-hidden pt-0 pl-0 pr-5 pb-5 ">
+            <h1>Testimonials</h1>
+            <div className="border w-40 h-1 bg-green-600 mt-7 ml-7 mr-auto mb-auto"></div>
+            <div className="row flex flex-wrap justify-center">
+              <div className="colcss box-border p-4">
+                <div className="testimonial bg-slate-800 p-8">
+                  <img className=" w-24 h-24 rounded-full" src={p1} alt="john" />
+                  <div className="name">Full Name</div>
+                  <div className="star">
+                    <FontAwesomeIcon className=" text-xs" icon={faStar} />
+                    <FontAwesomeIcon className=" text-xs" icon={faStar} />
+                    <FontAwesomeIcon className=" text-xs" icon={faStar} />
+                    <FontAwesomeIcon className=" text-xs" icon={faStar} />
+                    <FontAwesomeIcon className=" text-xs" icon={faStar} />
+                    <FontAwesomeIcon className=" text-xs" icon={faStar} />
+                  </div>
+                  <p>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                    Minima atque eum neque et ex, quibusdam porro. Obcaecati
+                    libero, possimus dolorem, corporis inventore sequi sit quos
+                    aliquam adipisci quibusdam reprehenderit dolore.
+                  </p>
+                </div>
+              </div>{" "}
+              {/* <div className="col">
+                <div className="testimonial">
+                  <img src={p1} alt="" /> 
+                  <div className="name">Full Name</div>
+                  <div className="star">
+                    <FontAwesomeIcon className=" text-xs" icon={faStar} />
+                    <FontAwesomeIcon className=" text-xs" icon={faStar} />
+                    <FontAwesomeIcon className=" text-xs" icon={faStar} />
+                    <FontAwesomeIcon className=" text-xs" icon={faStar} />
+                    <FontAwesomeIcon className=" text-xs" icon={faStar} />
+                    <FontAwesomeIcon className=" text-xs" icon={faStar} />
+                  </div>
+                  <p>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                    Minima atque eum neque et ex, quibusdam porro. Obcaecati
+                    libero, possimus dolorem, corporis inventore sequi sit quos
+                    aliquam adipisci quibusdam reprehenderit dolore.
+                  </p>
+                </div>
+              </div> */}{" "}
+              {/* <div className="col">
+                <div className="testimonial">
+                  <img src={p1} alt="" />
+                  <div className="name">Full Name</div>
+                  <div className="star">
+                    <FontAwesomeIcon className=" text-xs" icon={faStar} />
+                    <FontAwesomeIcon className=" text-xs" icon={faStar} />
+                    <FontAwesomeIcon className=" text-xs" icon={faStar} />
+                    <FontAwesomeIcon className=" text-xs" icon={faStar} />
+                    <FontAwesomeIcon className=" text-xs" icon={faStar} />
+                    <FontAwesomeIcon className=" text-xs" icon={faStar} />
+                  </div>
+                  <p>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                    Minima atque eum neque et ex, quibusdam porro. Obcaecati
+                    libero, possimus dolorem, corporis inventore sequi sit quos
+                    aliquam adipisci quibusdam reprehenderit dolore.
+                  </p>
+                </div>
+              </div> */}
+            </div>
+          </div>
         </div>
         <div>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi
